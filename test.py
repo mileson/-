86e7f06d9c27d9840a24e8557440d42310db6971
libingfeng <libingfeng007@gmail.com>
2018-04-19 23:57:24 +0800

    new

---
+++ test.py
@@ -0,0 +1,18 @@
+#! usr/bin/env pyhton3
+# -*- coding:utf-8 -*-
+
+import math
+
+def quadratic(a, b, c):
+	
+	result = math.sqrt(((-c-b)/a))
+
+	return result
+
+
+print(quadratic(2,3,1))
+
+
+
+
+
